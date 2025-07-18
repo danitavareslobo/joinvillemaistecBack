cfea34efc847d830f19a19e1a4848f5972561eef
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2025-07-18 11:06:30 -0300

    Ex 4 – Controle de estoque: Reposição

--- Trello/Semana2/controle-estoque/src/Main.java
+++ Trello/Semana2/controle-estoque/src/Main.java
@@ -60,7 +60,33 @@ public class Main {
                     }
                     break;
                 case 2:
-                    System.out.println("Opção Reposição selecionada.");
+                    System.out.println("\n=== REPOSIÇÃO ===");
+                    System.out.println("Produtos disponíveis:");
+                    System.out.println("[1] " + nomeProduto1 + " - Estoque: " + quantidadeProduto1);
+                    System.out.println("[2] " + nomeProduto2 + " - Estoque: " + quantidadeProduto2);
+                    System.out.println("[3] " + nomeProduto3 + " - Estoque: " + quantidadeProduto3);
+                    System.out.print("Qual produto deseja repor? ");
+
+                    int produtoReposicao = scanner.nextInt();
+                    System.out.print("Quantidade: ");
+                    int quantidadeReposicao = scanner.nextInt();
+
+                    switch (produtoReposicao) {
+                        case 1:
+                            quantidadeProduto1 += quantidadeReposicao;
+                            System.out.println("Reposição realizada com sucesso! " + quantidadeReposicao + " " + nomeProduto1 + " adicionados ao estoque.");
+                            break;
+                        case 2:
+                            quantidadeProduto2 += quantidadeReposicao;
+                            System.out.println("Reposição realizada com sucesso! " + quantidadeReposicao + " " + nomeProduto2 + " adicionados ao estoque.");
+                            break;
+                        case 3:
+                            quantidadeProduto3 += quantidadeReposicao;
+                            System.out.println("Reposição realizada com sucesso! " + quantidadeReposicao + " " + nomeProduto3 + " adicionados ao estoque.");
+                            break;
+                        default:
+                            System.out.println("Produto inválido!");
+                    }
                     break;
                 case 3:
                     System.out.println("Opção Relatório selecionada.");
