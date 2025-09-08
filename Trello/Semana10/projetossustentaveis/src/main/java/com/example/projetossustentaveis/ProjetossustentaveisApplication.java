9d74dffabc9c5e4b493743b578736c2ad869cd93
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2025-09-08 10:40:14 -0300

    [M2S11] Ex 4 - Deploy

---
+++ Trello/Semana10/projetossustentaveis/src/main/java/com/example/projetossustentaveis/ProjetossustentaveisApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetossustentaveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetossustentaveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetossustentaveisApplication.class, args);
+	}
+
+}
---
+++ Trello/Semana10/projetossustentaveis/src/test/java/com/example/projetossustentaveis/ProjetossustentaveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetossustentaveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetossustentaveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
